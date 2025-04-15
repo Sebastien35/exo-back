a3172d1be3a61895d8d8acea8ac8b574796679dc
Sebastien35 <Sborgne@live.fr>
2025-04-15 09:55:01 +0200

    dsl robin

--- src/modules/consultation.module.ts
+++ src/modules/consultation.module.ts
@@ -3,10 +3,11 @@ import { ConsultationService } from '../services/consultation.service';
 import { ConsultationController } from '../controllers/consultation.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Consultation } from '../entity/consultation.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Consultation])],
-  providers: [ConsultationService],
+  imports: [TypeOrmModule.forFeature([Consultation]), JwtModule],
+  providers: [ConsultationService, ],
   controllers: [ConsultationController],
 })
 export class ConsultationModule {}
